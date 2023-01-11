f16867cd6bd4224334fd8fcfd6311c71b2883f7d
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-01-11 19:46:57 +0530

    Update merge two sorted array.cpp

--- leetcode/merge two sorted array.cpp
+++ leetcode/merge two sorted array.cpp
@@ -40,30 +40,31 @@ void merged(int a[],int n,int b[],int m,int c[])
 
 }
 
+void print(int a[],int n)
+{
+    for (int i=0;i<n;i++)
+    {
+        cout<<a[i]<<"  ";
+    }
+}
+
 int main()
 {
     int a[3]={2,5,7};
     int b[4]={1,3,4,6};
     int c[7]={0};
-    cout<<"1st:  ";
 
-    for (int i=0;i<3;i++)
-    {
-        cout<<a[i]<<"  ";
-    }
+    int n=3,m=4;
+
+    cout<<"1st:  ";
+    print(a,n);
 
     cout<<endl<<"2nd:  ";
-    for (int i=0;i<4;i++)
-    {
-        cout<<b[i]<<"  ";
-    }
+    print(b,m);
 
     merged(a,3,b,4,c);
     cout<<endl<<"merge:  ";
-    for (int i=0;i<7;i++)
-    {
-        cout<<c[i]<<"  ";
-    }
+    print(c,m+n);
 
     return 0;
 }
