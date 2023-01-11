bf638aae5e5030c9fc1e3c7233e28a1371b0a291
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-01-11 19:40:43 +0530

    Add files via upload

---
+++ leetcode/merge two sorted array.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+//#include <array>
+using namespace std;
+
+int c [10];
+
+void merged(int a[],int n,int b[],int m)
+{
+    int i=0,j=0,k=0;
+    while(i<n && j<m)
+    {
+        if(a[i]<b[j])
+        {
+            c[k]=a[i];
+            i++;
+
+        }
+        else if(a[i]>b[j])
+        {
+            c[k]=b[j];
+            j++;
+        }
+        else
+        {
+            c[k]=b[j];
+            i++;
+            j++;
+        }
+        k++;
+    }
+    while(i<n)
+    {
+        c[k]=a[i];
+        i++;
+        k++;
+    }
+    while(j<m)
+    {
+        c[k]=b[j];
+        j++;
+        k++;
+    }
+
+
+}
+
+int main()
+{
+    int a[3]={2,5,7};
+    int b[4]={1,3,4,6};
+    cout<<"1st:  ";
+
+    for (int i=0;i<3;i++)
+    {
+        cout<<a[i]<<"  ";
+    }
+
+    cout<<endl<<"2nd:  ";
+    for (int i=0;i<4;i++)
+    {
+        cout<<b[i]<<"  ";
+    }
+
+    merged(a,3,b,4);
+    cout<<endl<<"merge:  ";
+    for (int i=0;i<7;i++)
+    {
+        cout<<c[i]<<"  ";
+    }
+
+    return 0;
+}
