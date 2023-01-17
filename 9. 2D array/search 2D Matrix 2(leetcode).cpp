c26694c02bb3de799adbff10b4a0bc7498f826cf
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-01-17 20:24:58 +0530

    Create search 2D Matrix 2(leetcode).cpp

---
+++ 9. 2D array/search 2D Matrix 2(leetcode).cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int row=matrix.size();
+        int col=matrix[0].size();
+
+        int rowIndex=0;
+        int colIndex=col-1;
+
+        while(rowIndex < row && 0<=colIndex)
+        {
+            int ele=matrix[rowIndex][colIndex];
+            if(ele==target)
+            {
+                return 1;
+            }
+            else if(ele<target)
+            {
+                rowIndex++;
+            }
+            else
+            {
+                colIndex--;
+            }
+        }
+        return 0;
+    }
+};
