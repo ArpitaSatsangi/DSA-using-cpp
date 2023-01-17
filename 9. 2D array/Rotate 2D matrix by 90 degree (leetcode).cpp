8ddedac051ddc6a9e1246e74be5c93f8800185d3
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-01-17 20:31:30 +0530

    Create Rotate 2D matrix by 90 degree (leetcode).cpp

---
+++ 9. 2D array/Rotate 2D matrix by 90 degree (leetcode).cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int total=(row*col)-1;
+        int count=0;
+
+        int startingRow=0;   
+        int startingCol=0;   
+        int endingRow=row-1;     
+        int endingCol=col-1;    
+
+        while(count<total)
+        {
+            .
+            .
+            .
+            .
+            .
+
+        }
+        .
+        .
+    }
+};
