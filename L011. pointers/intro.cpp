f679ac3888e21294061b4dcce357d81fcb4a6537
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-01-20 23:18:32 +0530

    Create intro.cpp

---
+++ L011. pointers/intro.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=8;
+    int *ptr=&a;
+
+    cout<<"value of a = "<<a<<endl;
+    cout<<"value of a = "<<*ptr<<endl<<endl;
+
+    cout<<"address of a = "<<&a<<endl;
+    cout<<"address of a = "<<ptr<<endl;
+
+    return 0;
+}
