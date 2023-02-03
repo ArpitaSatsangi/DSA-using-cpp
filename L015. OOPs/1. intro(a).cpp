ff494bcd0af6eddbf52b3396f6e9e7bd84f0707f
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-02-03 18:40:26 +0530

    Create 1. intro(a).cpp

---
+++ L015. OOPs/1. intro(a).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "Hero.cpp"
+using namespace std;
+
+class Heroine {//class definition
+
+    public:
+
+    char level;
+    int health;
+
+};
+
+
+class Villain {
+    //empty class;
+};
+
+
+int main()
+{
+    //creation of object
+    Heroine Layla;
+
+    Layla.health=70;
+    Layla.level='A';
+
+    cout<<"size of Heroine Layla: "<<sizeof(Layla)<<endl;//8
+    cout<<"health is "<<Layla.health<<endl;
+    cout<<"level is "<<Layla.level<<endl;
+
+    Villain Sam;
+    cout<<endl<<endl<<"size of Villain Sam: "<<sizeof(Sam)<<endl;//1
+
+    Hero Adam;
+    cout<<endl<<"size of Hero Adam: "<<sizeof(Adam)<<endl;//28
+
+    return 0;
+}
