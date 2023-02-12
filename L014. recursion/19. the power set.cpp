6b85b89d8cb29916d0dd18ace7e57a3d716afe01
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-02-12 23:10:54 +0530

    Create 19. the power set.cpp

---
+++ L014. recursion/19. the power set.cpp
@@ -0,0 +1,33 @@
+//Given an integer array nums of unique elements, return all possible subsets(the power set).
+
+#include <vector>
+class Solution {
+
+private:
+    void solve(vector<int>& nums, vector <vector <int> > & ans, vector <int>output,int index)
+    {
+        if(index >= nums.size())
+        {
+            ans.push_back(output);
+            return;
+        }
+
+        //exclude
+        solve(nums,ans,output,index+1);
+
+        //include
+        int ele=nums[index];
+        output.push_back(ele);
+        solve(nums,ans,output,index+1);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector <vector <int> > ans;
+        vector <int> output;
+        int index=0;
+
+        solve(nums,ans,output,index);
+        return ans;
+    }
+};
