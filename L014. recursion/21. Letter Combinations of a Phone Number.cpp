cddae187a4f40e79380f6cb14117c91afdac4f70
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-02-14 19:20:58 +0530

    Create 21. Letter Combinations of a Phone Number.cpp

---
+++ L014. recursion/21. Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    void solve(vector <string> & ans, string output, int index,string digits,string mapping[])
+    {
+        if(index >= digits.length())
+        {
+            ans.push_back(output);
+            return;
+        }
+
+        int value = digits[index] -'0';
+        string elements = mapping[value] ;
+
+        for(int i=0; i<elements.length();i++)
+        {
+            output.push_back(elements[i]);
+            solve(ans, output, index+1, digits, mapping);
+            output.pop_back();
+        }
+    }
+
+
+public:
+    vector<string> letterCombinations(string digits) {
+
+        vector <string> ans;
+        string output="";
+        if(digits.length()==0)
+        {
+            return ans;
+        }
+
+        int index=0;
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl" ,"mno", "pqrs", "tuv", "wxyz"};
+
+        solve(ans,output,index,digits,mapping);
+
+        return ans;
+    }
+};
