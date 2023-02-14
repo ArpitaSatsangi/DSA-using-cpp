258f62d6d14eea9507d3884914ab819cd3831e35
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-02-14 19:48:17 +0530

    Update 21. Letter Combinations of a Phone Number.cpp

--- L014. recursion/21. Letter Combinations of a Phone Number.cpp
+++ L014. recursion/21. Letter Combinations of a Phone Number.cpp
@@ -1,3 +1,11 @@
+/*
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that 
+the number could represent. Return the answer in any order.
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+*/
+
 class Solution {
 private:
     void solve(vector <string> & ans, string output, int index,string digits,string mapping[])
