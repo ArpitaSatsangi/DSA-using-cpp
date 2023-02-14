681482c6d3e6fb2301417d5c24c0bc63feb14c93
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-02-14 19:49:23 +0530

    Update 19. the power set.cpp

--- L014. recursion/19. the power set.cpp
+++ L014. recursion/19. the power set.cpp
@@ -1,4 +1,9 @@
-//Given an integer array nums of unique elements, return all possible subsets(the power set).
+/*
+Given an integer array 'nums' of unique elements, return all possible subsets(the power set).
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+*/
 
 #include <vector>
 class Solution {
