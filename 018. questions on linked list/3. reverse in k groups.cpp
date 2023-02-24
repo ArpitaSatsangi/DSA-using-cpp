c4a0e1491678476339974ad607b6cc4f4ac437b0
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-02-24 18:36:11 +0530

    Add files via upload

---
+++ 018. questions on linked list/3. reverse in k groups.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int d)
+    {
+        this->data=d;
+        this->next=NULL;
+    }
+
+};
+
+node * kreverse(node * &head,int k)
+{
+    if(head==NULL)
+    {
+        return NULL;
+    }
+
+    node *next=NULL;
+    node *current=head;
+    node *prev=NULL;
+    int count=0;
+
+    while(current!=NULL && count<k)
+    {
+        next=current->next;
+        current->next=prev;
+        prev=current;
+        current=next;
+        count++;
+    }
+
+    if(next!=NULL)
+        head->next=kreverse(next,k);
+
+
+    return prev;
+}
+
+
+void insertStart(node * &head,int d)
+{
+    //new node is created
+    node *temp=new node(d);
+    temp->next=head;
+    head=temp;
+}
+
+void insertTail(node * &tail,int d)
+{
+    node *temp=new node(d);
+    tail->next=temp;
+    tail=temp;
+}
+
+void traverse(node * head)
+{
+    node *temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<"  ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    node *n1=new node(1);
+    node *head=n1;
+    node *tail=n1;
+
+    insertStart(head,4);
+    insertStart(head,0);
+    insertStart(head,5);
+
+    insertTail(tail,3);
+    insertTail(tail,6);
+    insertTail(tail,9);
+    insertTail(tail,2);
+
+    traverse(head);
+
+    node *newww = kreverse(head,3);
+    cout<<endl<<"group of three:"<<endl;
+    traverse(newww);
+
+    return 0;
+}
+
+
