065c8263f3aae8c27eb820f9ea54c0acfac25852
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-03-01 12:11:27 +0530

    Create 3. two stacks.cpp

---
+++ 019. Stack/3. two stacks.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h> 
+class TwoStack {
+
+public:
+
+    // Initialize TwoStack.
+    int *arr;
+    int top1;
+    int top2;
+    int size;
+
+    TwoStack(int s) {
+        // Write your code here.
+        this->size=s;
+        top1=-1;
+        top2=s;
+        arr=new int[s];
+    }
+    
+    // Push in stack 1.
+    void push1(int num) {
+        // Write your code here
+        if(top2-top1>1)
+        {
+            top1++;
+            arr[top1]=num;
+        }
+    }
+
+    // Push in stack 2.
+    void push2(int num) {
+        // Write your code here.
+        if(top2-top1>1)
+        {
+            top2--;
+            arr[top2]=num;
+        }
+    }
+
+    // Pop from stack 1 and return popped element.
+    int pop1() {
+        // Write your code here.
+        
+        if(top1>=0)
+        {
+            int x=arr[top1];
+            top1--;
+            return x;
+        }
+        else
+        {
+
+            return -1;
+        }
+    }
+
+    // Pop from stack 2 and return popped element.
+    int pop2() {
+        // Write your code here.
+        if(top2<size)
+        {
+            int x=arr[top2];
+            top2++;
+            return x;
+        }
+        else
+        {
+
+            return -1;
+        }
+    }
+};
+#include <bits/stdc++.h> 
+class TwoStack {
+
+public:
+
+    // Initialize TwoStack.
+    int *arr;
+    int top1;
+    int top2;
+    int size;
+
+    TwoStack(int s) {
+        // Write your code here.
+        this->size=s;
+        top1=-1;
+        top2=s;
+        arr=new int[s];
+    }
+    
+    // Push in stack 1.
+    void push1(int num) {
+        // Write your code here
+        if(top2-top1>1)
+        {
+            top1++;
+            arr[top1]=num;
+        }
+    }
+
+    // Push in stack 2.
+    void push2(int num) {
+        // Write your code here.
+        if(top2-top1>1)
+        {
+            top2--;
+            arr[top2]=num;
+        }
+    }
+
+    // Pop from stack 1 and return popped element.
+    int pop1() {
+        // Write your code here.
+        
+        if(top1>=0)
+        {
+            int x=arr[top1];
+            top1--;
+            return x;
+        }
+        else
+        {
+
+            return -1;
+        }
+    }
+
+    // Pop from stack 2 and return popped element.
+    int pop2() {
+        // Write your code here.
+        if(top2<size)
+        {
+            int x=arr[top2];
+            top2++;
+            return x;
+        }
+        else
+        {
+
+            return -1;
+        }
+    }
+};
