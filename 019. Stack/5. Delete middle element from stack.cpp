84932911966f4caabceb0e1c5e0ab11366b10bcc
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-08-17 18:53:23 +0530

    Create 5. Delete middle element from stack.cpp

---
+++ 019. Stack/5. Delete middle element from stack.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+
+void solve(stack<int>&inputStack, int count, int N)
+{
+   if(count== N/2)
+   {
+      inputStack.pop();
+      return ;
+   }
+
+   int num = inputStack.top();
+   inputStack.pop();
+
+   solve(inputStack, count+1, N);
+
+   inputStack.push(num);
+
+}
+
+void deleteMiddle(stack<int>&inputStack, int N){
+	
+   int count=0;
+   solve(inputStack, count, N);
+   
+}
