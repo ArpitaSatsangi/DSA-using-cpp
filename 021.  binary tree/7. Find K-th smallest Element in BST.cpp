cc13d837159b70579c1263b66fa6e27d3a298eaa
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-09-18 18:53:35 +0530

    Create 7. Find K-th smallest Element in BST.cpp

---
+++ 021.  binary tree/7. Find K-th smallest Element in BST.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+ 
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode 
+    {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+int solve(BinaryTreeNode<int>* root, int k, int &i)
+{
+    if(root==NULL)
+    {
+        return -1;
+    }
+     
+    int left = solve(root->left, k, i);
+
+    
+    if(left!=-1)
+    {
+        return left;
+    }
+
+    i++;
+    if(i==k)
+    {
+        return root->data;
+    }
+
+    return solve(root->right, k ,i);
+
+}
+
+int kthSmallest(BinaryTreeNode<int>* root, int k) {
+    
+    int i = 0;
+    return solve(root, k, i);
+
+}
