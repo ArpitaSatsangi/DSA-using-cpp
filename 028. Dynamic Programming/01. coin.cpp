cc445b91e7cb16fb95a9e3dcea6eb5f4c8759d0a
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2024-01-18 20:54:10 +0530

    Create 01. coin.cpp

---
+++ 028. Dynamic Programming/01. coin.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int count(vector<int>& coins, int n, int sum)
+{
+	vector<vector<int> > dp(n + 1, vector<int>(sum + 1, 0));
+
+	dp[0][0] = 1;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j <= sum; j++) {
+
+			// Add the number of ways to make change without
+			// using the current coin,
+			dp[i][j] += dp[i - 1][j];
+
+			if ((j - coins[i - 1]) >= 0) {
+
+				// Add the number of ways to make change
+				// using the current coin
+				dp[i][j] += dp[i][j - coins[i - 1]];
+			}
+		}
+	}
+	return dp[n][sum];
+}
+
+int main()
+{
+	vector<int> coins{ 1, 2, 3 };
+	int n = 3;
+	int sum = 5;
+	cout << count(coins, n, sum);
+	return 0;
+}
